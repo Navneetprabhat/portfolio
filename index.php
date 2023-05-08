a0f94abea13f93c5b2ca31d5dfd0f05c0f94b227
Navneet <48380568+Navneetprabhat@users.noreply.github.com>
2023-05-08 23:10:34 +0530

    added the php files

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>
+      Onkar Deshpande
+    </title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="11.png" />
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link rel="stylesheet" href="font.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" type="image/png" href="download.png" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+  
+
+
+<nav class="white" role="navigation">
+    <div class="nav-wrapper">
+      <a id="navbar-brand" href="https://www.cse.iitb.ac.in/~onkard/" class="brand-logo">Onkar Deshpande</a>
+      <ul class="right hide-on-med-and-down small-text">
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/aboutme.html"><h3>About Me</h3></a></li>
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/acads.html"><h3>Acads</h3></a></li>
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/CS251_Team/"><h3>CS 251</h3></a></li>
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/projects.html"><h3>Projects</h3></a></li>
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/contact.html"><h3>Contact me</h3></a></li>
+        <li class="dropdown">
+        	<a class="dropdown-toggle" data-toggle="dropdown" href="#"><h3>Résumé</h3>
+        	</a>
+        	<ul class="dropdown-menu">
+          		<li><a href="https://www.cse.iitb.ac.in/~onkard/1page.pdf" target="_blank"><h4>1 page</h4></a></li>
+          		<li><a href="https://www.cse.iitb.ac.in/~onkard/2pagetech.pdf" target="_blank"><h4>2 page(Tech)</h4></a></li>
+          		<li><a href="https://www.cse.iitb.ac.in/~onkard/2pagenontech.pdf" target="_blank"><h4>2 page(non-Tech)</h4></a></li>
+        	</ul>
+      	</li>
+      	<li><a href="https://www.cse.iitb.ac.in/~onkard/feedback.php"><h3>Feedback</h3></a></li>
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/review.php"><h3>Reviews</h3></a></li>
+      </ul>
+
+      <ul id="nav-mobile" class="sidenav">
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/aboutme.html"><h4>About Me</h4></a></li>
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/acads.html"><h4>Acads</h4></a></li>
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/CS251_Team/"><h4>CS 251</h4></a></li>
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/projects.html"><h4>Projects</h4></a></li>
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/contact.html"><h4>Contact me</h4></a></li>
+        <li class="dropdown">
+        	<a class="dropdown-toggle" data-toggle="dropdown" href="#"><h4>Résumé</h4>
+        	</a>
+        	<ul class="dropdown-menu">
+          		<li><a href="https://www.cse.iitb.ac.in/~onkard/1page.pdf" target="_blank"><h4>1 page</h4></a></li>
+          		<li><a href="https://www.cse.iitb.ac.in/~onkard/2pagetech.pdf" target="_blank"><h4>2 page(Tech)</h4></a></li>
+          		<li><a href="https://www.cse.iitb.ac.in/~onkard/2pagenontech.pdf" target="_blank"><h4>2 page(non-Tech)</h4></a></li>
+        	</ul>
+      	</li>
+      	<li><a href="https://www.cse.iitb.ac.in/~onkard/feedback.php"><h4>Feedback</h4></a></li>
+        <li><a href="https://www.cse.iitb.ac.in/~onkard/review.php"><h4>Reviews</h4></a></li>
+      </ul>
+      <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+  </nav>
+<br /><br /><br /><br />
+
+<div class="container center">
+<blockquote>If you can fill the unforgiving minute<br />
+    With sixty seconds’ worth of distance run,<br />
+    Yours is the Earth and everything that’s in it,<br />
+    And—which is more—you’ll be a Man, my son!<br />
+</blockquote>
+From <a href="https://www.poetryfoundation.org/poems/46473/if---">'If'</a> by Rudyard Kipling
+</div>
+<br /><br /><br /><br />
+
+  <div id="profile" style="font-family:HelveticaNeue; font-weight:900;">
+    <div id="img" align="center" style="padding-left:3%">
+      <div class="container"  style="width:800px; height:400px">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+            <li data-target="#myCarousel" data-slide-to="4"></li>
+          </ol>
+
+    <!-- Wrapper for slides -->
+          <div class="carousel-inner">
+            <div class="item active">
+              <img src="30.jpg" alt="Gurkul" align="middle">
+            </div>
+            <div class="item">
+              <img src="31.jpg" alt="CSE Dept Trek" align="middle">
+            </div>
+    
+            <div class="item">
+              <img src="32.jpg" alt="Wingies" align="middle">
+            </div>
+            <div class="item">
+              <img src="33.jpg" alt="Enigm" align="middle">
+            </div>
+            <div class="item">
+              <img src="34.jpg" alt="Lonavla Waterpark" align="middle">
+            </div>
+          </div>
+
+    <!-- Left and right controls -->
+          <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <div class="center">
+      <a class="btn waves-effect waves-teal modal-trigger black" href="#modal1">True to me!!</a>
+    </div>
+  </div>
+  <div id="modal1" class="modal">
+  
+  	<!--<embed src="sm.mp4" width="100%" height="100%" />-->
+  	
+    <!--<video src="sm.mp4" controls height="100%" width="100%">
+</video>-->
+
+
+
+      <h4>So you want to know more?</h4>
+      <p>I am glad. But what can I say? I was born in Aurangabad. Studied There until I got into IITB. And right now I am pursuing a B.Tech. with a major in Computer Science. If you want to know me better, just head to 'About me'.<br>Cheers!</p>
+    
+  </div>
+  <?php
+
+      /* counter */
+
+      //opens countlog.txt to read the number of hits
+      $datei = fopen("countlog.txt","r");
+      $count = fgets($datei,1000);
+      fclose($datei);
+      $count = $count + 1 ;
+      $a = $count;
+      // opens countlog.txt to change new hit number
+      $datei = fopen("countlog.txt","w");
+      fwrite($datei, $count);
+      fclose($datei);
+
+    ?>
+    <br /><br /><br /><br />
+    <br /><br /><br /><br />
+
+   
+
+
+
+  <footer class="page-footer  grey darken-3">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h2 class="white-text">Gratitude turns what we have into enough</h2>
+          <p class="grey-text text-lighten-4">Thank You for visiting this page. It meant a lot to me. We are glad to tell you that you are not alone. This page has been visited <i id="abc">__</i> times. </p>
+        </div>   
+      </div>
+    </div>
+  </footer>
+<script type="text/javascript">
+  var o = <?=$a?>;
+  document.getElementById('abc').innerHTML = o;
+    document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+  </script>
+</body>
+</html>
